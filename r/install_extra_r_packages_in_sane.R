db258f4056809f80ca57ede6b62b4f4f83bde2de
Qixiang Fang <fqixiang@gmail.com>
2026-06-05 16:26:11 +0200

    Use variable for package folder and install dsl
    Introduce package_folder_path to centralize the extra R packages directory, update list.files to use this variable, and add an explicit install step for the 'dsl' package (installed as a source package). CRAN binary .zip packages are still installed with type = 'win.binary'. Also minor comment wording cleanup.

--- r/install_extra_r_packages_in_sane.R
+++ r/install_extra_r_packages_in_sane.R
@@ -1,8 +1,11 @@
-# The following code installs the extra R packages for SICSS 2026.
+# The following code installs extra R packages for SICSS 2026.
 # It is meant to be run within the SANE environment.
 
+package_folder_path <- "S:/scripts/install_extra_r_packages_for_sicss26/extra_r_packages_for_sane"
+
+# Install the CRAN binary packages first
 files <- list.files(
-  "S:/scripts/install_extra_r_packages_for_sicss26/extra_r_packages_for_sane",
+  package_folder_path,
   pattern = "\\.zip$",
   full.names = TRUE
 )
@@ -11,4 +14,16 @@ install.packages(
   files,
   repos = NULL,
   type = "win.binary"
+)
+
+# Install the `dsl` package, which is only available as a source package
+dsl_package_path <- file.path(
+  package_folder_path,
+  "dsl.zip"
+)
+
+install.packages(
+  dsl_package_path,
+  repos = NULL,
+  type = "source"
 )
