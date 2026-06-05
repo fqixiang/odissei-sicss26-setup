cc7b427da120364426b972ddc7e7eba62657d445
Qixiang Fang <fqixiang@gmail.com>
2026-06-05 18:26:53 +0200

    Reorganize extra R package download/install dirs
    Move scripts into extra_r_packages_for_sane/ and restructure how downloaded packages are stored. Create a main downloaded_r_packages folder with cran/ and source/ subdirectories, update download_extra_r_packages_for_sane.R to save CRAN binaries to cran/ and the dsl ZIP to source/, and update install_extra_r_packages_in_sane.R to point to the new S:/.../extra_r_packages_for_sane/downloaded_r_packages path and use the cran/ and source/ subfolders when installing.

--- extra_r_packages_for_sane/download_extra_r_packages_for_sane.R
+++ extra_r_packages_for_sane/download_extra_r_packages_for_sane.R
@@ -6,9 +6,12 @@ pkgs <- c("grf", "estimatr", "SuperLearner", "arm", "matrixcalc",
           "sf", "terra", "tidyterra", "lwgeom", "tmap", "leaflet",
           "mapview", "spdep", "spatialreg", "sfdep")
 
-dir_name <- "extra_r_packages_for_sane"
-dir_path <- here::here("r", dir_name)
-dir.create(dir_path, showWarnings = FALSE)
+main_pkg_dir_name <- "downloaded_r_packages"
+main_pkg_dir_path <- here::here("extra_r_packages_for_sane", main_pkg_dir_name)
+cran_pkg_dir_path <- file.path(main_pkg_dir_path, "cran")
+source_pkg_dir_path <- file.path(main_pkg_dir_path, "source")
+dir.create(cran_pkg_dir_path, showWarnings = FALSE)
+dir.create(source_pkg_dir_path, showWarnings = FALSE)
 
 ap <- available.packages(
   repos = "https://cloud.r-project.org",
@@ -27,7 +30,7 @@ all_pkgs <- setdiff(all_pkgs, rownames(installed.packages(priority = "base")))
 
 download.packages(
   all_pkgs,
-  destdir = dir_path,
+  destdir = cran_pkg_dir_path,
   repos = "https://cloud.r-project.org",
   type = "win.binary"
 )
@@ -35,6 +38,6 @@ download.packages(
 # Download the `dsl` package from GitHub
 download.file(
   "https://github.com/naoki-egami/dsl/archive/refs/heads/master.zip",
-  destfile = file.path(dir_path, "dsl.zip"),
+  destfile = file.path(source_pkg_dir_path, "dsl.zip"),
   mode = "wb"
 )
--- extra_r_packages_for_sane/install_extra_r_packages_in_sane.R
+++ extra_r_packages_for_sane/install_extra_r_packages_in_sane.R
@@ -1,11 +1,11 @@
 # The following code installs extra R packages for SICSS 2026.
 # It is meant to be run within the SANE environment.
 
-package_folder_path <- "S:/scripts/install_extra_r_packages_for_sicss26/extra_r_packages_for_sane"
+package_main_folder_path <- "S:/scripts/extra_r_packages_for_sane/downloaded_r_packages"
 
 # Install the CRAN binary packages first
 files <- list.files(
-  package_folder_path,
+  file.path(package_main_folder_path, "cran"),
   pattern = "\\.zip$",
   full.names = TRUE
 )
@@ -18,7 +18,7 @@ install.packages(
 
 # Install the `dsl` package, which is only available as a source package
 dsl_package_path <- file.path(
-  package_folder_path,
+  file.path(package_main_folder_path, "source"),
   "dsl.zip"
 )
 
